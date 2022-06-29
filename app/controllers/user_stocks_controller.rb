c6e9cb3841c81f550158c5b137eb8247869e8b55
Gabriel Valente <gabrielmcv1@gmail.com>
2022-06-29 16:27:07 +0100

    User can stop tracking stocks

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -12,4 +12,12 @@ class UserStocksController < ApplicationController
     flash[:notice] = "Stock #{stock.name} was added to your portfolio."
     redirect_to portfolio_path
   end
+
+  def destroy
+    stock = Stock.find(params[:id])
+    UserStock.where(user_id: current_user.id, stock_id: params[:id]).first.destroy
+    
+    flash[:notice] = "Stock #{stock.ticker} was removed from your portfolio."
+    redirect_to portfolio_path
+  end
 end
