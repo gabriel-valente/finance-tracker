4e26bcf71939763292c02e7fe0c61bc216fca40c
Gabriel Valente <gabrielmcv1@gmail.com>
2022-06-29 15:19:31 +0100

    Users can track stocks

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,9 @@
 class Stock < ApplicationRecord
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
+
+  validates :name, :ticker, presence: true
+
   def self.new_lookup(ticker_symbol)
     client = IEX::Api::Client.new(
       publishable_token: Rails.application.credentials.iex[:publishable],
@@ -11,5 +16,9 @@ class Stock < ApplicationRecord
     rescue => exception
       return nil
     end
+  end
+
+  def self.check_db(ticker)
+    stock = where(ticker: ticker).first
   end
 end
