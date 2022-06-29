de6768e507cfe06ae4154c785a2fab5b17860cdc
Gabriel Valente <gabrielmcv1@gmail.com>
2022-06-29 12:07:43 +0100

    API requests with AJAX

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,6 +1,18 @@
 class StocksController < ApplicationController
   def search
-    stock = Stock.new_lookup(params[:stock])
-    render json: stock
+    if params[:stock].present?
+      @stock = Stock.new_lookup(params[:stock])
+      if @stock
+        respond_to do |format|
+          format.js { render partial: "users/result" }
+        end
+      else
+        flash[:alert] = "Please enter a valid symbol."
+        redirect_to portfolio_path
+      end
+    else
+      flash[:alert] = "Please enter a symbol to search."
+      redirect_to portfolio_path
+    end
   end
 end
